93b3ccd29fa224a41dfe0a603ae388b3c7342be8
hocine bouarara <hocine@univ-tiaret.dz>
2025-11-17 15:57:41 +0100

    Initial project structure

---
+++ src/main/java/com/hocinebouarara/Personal_Finance_App/PersonalFinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.hocinebouarara.Personal_Finance_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hocinebouarara/Personal_Finance_App/PersonalFinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hocinebouarara.Personal_Finance_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
